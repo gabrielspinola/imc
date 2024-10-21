743c0f96b78557a0195c723a32ab3c45359bb7df
Gabriel Spinola <gspinola@gmail.com>
2024-10-20 22:57:17 -0300

    Mudanças nas rotas e criação da Rota de usuário

--- backend/src/server.py
+++ backend/src/server.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_cors import CORS
-from src.route import route_bp
+from src.routes.route_receitas import route_receitas
+from src.routes.route_user import route_user
 
 app = Flask(__name__)
 CORS(app)
 
-app.register_blueprint(route_bp)
+app.register_blueprint(route_receitas)
+app.register_blueprint(route_user)
